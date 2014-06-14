544dedd0da3b6d8c103ef72fdc1a44e4c3dcbf34
timepp <tongjunhui@live.cn>
2014-06-14 18:21:09 +0800

    first commit

---
+++ openas/main.d
@@ -0,0 +1,47 @@
+import std.stdio;
+import std.string;
+import std.getopt;
+import std.file;
+import std.path;
+import std.process;
+
+void DeleteFiles(string dir, string pattern)
+{
+    auto files = std.file.dirEntries(dir, pattern, std.file.SpanMode.shallow);
+    foreach(f; files)
+    {
+        std.file.remove(f);
+    }
+}
+
+int main(string[] argv)
+{
+    bool showHelp = false;
+    getopt(argv, "help|h", &showHelp);
+
+    if (argv.length <= 1)
+    {
+        showHelp = true;
+    }
+
+    if (showHelp)
+    {
+        writeln("openas: save stream and open it as specific file type. \n\n"
+                "Usage: openas <extension>\n"
+                "\n"
+                "Example:\n"
+                "  echo 'a->b->c' | dot2dgml | openas dgml\n"
+                );
+        return 0;
+    }
+
+    string tempdir = std.file.tempDir();
+    DeleteFiles(tempdir, "openas_temp.*");
+
+    string fileName = std.path.buildPath(tempdir, "openas_temp." ~ argv[1]);
+    File(fileName, "w").writeln(readln(0));
+
+    std.process.browse(fileName);
+
+    return 0;
+}
